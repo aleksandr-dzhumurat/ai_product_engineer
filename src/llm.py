4c2b74bd8105186244937f35172a3544b77d3e56
Aleksandr Dzhumurat <adzhumurat@yandex.ru>
2025-10-12 19:27:29 +0200

    Notebooks update

--- src/llm.py
+++ src/llm.py
@@ -1,47 +1,39 @@
+import hashlib
 import os
 import time
-import datetime
-import hashlib
+from dataclasses import dataclass
 
 import backoff
 import openai
 from openai import OpenAI
 
 
-client = OpenAI(
-    api_key=os.environ["OPENAI_API_KEY"]
-)
+def get_openai_client(openai_key = None):
+    if openai_key is None:
+       openai_key = os.environ["OPENAI_API_KEY"]
+    client = OpenAI(
+        api_key=openai_key
+    )
+    return client
 
-def generate_id(seed=None):
+@dataclass
+class GenAIResponse:
+    text: str
+    total_tokens: str
+    model: str
+    generation_id: str
+
+
+def generate_id(seed=None, limit=None):
     if seed is None:
         seed = int(time.time())
     res = str(
         hashlib
         .md5(str(seed).encode('utf-8'))
         .hexdigest()
-    )[:8]
-    return res
-
-@backoff.on_exception(backoff.expo, openai.APIError)
-@backoff.on_exception(backoff.expo, openai.RateLimitError)
-@backoff.on_exception(backoff.expo,openai.Timeout)
-@backoff.on_exception(backoff.expo, RuntimeError)
-def gpt_query(gpt_params, verbose: bool = False, avoid_fuckup: bool = False) -> dict:
-    print('connecting OpenAI...')
-    if verbose:
-        print(gpt_params["messages"][1]["content"])
-    response = client.chat.completions.create(
-        **gpt_params
     )
-    gpt_response = response.choices[0].message.content
-    if avoid_fuckup:
-        if '[' in gpt_response or '?' in gpt_response or '{' in gpt_response:
-            raise RuntimeError
-    res = {'recs': gpt_response}
-    res.update({'completion_tokens': response.usage.completion_tokens, 'prompt_tokens': response.usage.prompt_tokens, 'total_tokens': response.usage.total_tokens})
-    seed_phrase = f'{str(datetime.datetime.now().timestamp())}{gpt_response}'
-    generation_id = generate_id(seed_phrase)
-    res.update({'id': generation_id})
+    if limit is not None:
+        res = res[:limit]
     return res
 
 def promt_generation(candidates):
@@ -57,32 +49,42 @@ def promt_generation(candidates):
   """
   return promt
 
-def generate(gpt_prompt, verbose=False):
+@backoff.on_exception(backoff.expo, openai.APIError)
+@backoff.on_exception(backoff.expo, openai.RateLimitError)
+@backoff.on_exception(backoff.expo,openai.Timeout)
+@backoff.on_exception(backoff.expo, RuntimeError)
+def generate(client, user_prompt, system_prompt = "You are a helpful assistant for business analytics", model="gpt-3.5-turbo"):
     gpt_params = {
-        'model': 'gpt-3.5-turbo',
-        'max_tokens': 500,
-        'temperature': 0.7,
-        'top_p': 0.5,
-        'frequency_penalty': 0.5,
+        "model": model,
+        "max_tokens": 500,
+        "temperature": 0.7,
+        "top_p": 0.5,
+        "frequency_penalty": 0.5,
     }
-    if verbose:
-        print(gpt_prompt)
     messages = [
         {
-          "role": "system",
-          "content": "You are a helpful assistant for business analytics",
+            "role": "system",
+            "content": system_prompt,
         },
         {
-          "role": "user",
-          "content": gpt_prompt,
+            "role": "user",
+            "content": user_prompt,
         },
     ]
-    gpt_params.update({'messages': messages})
-    res = gpt_query(gpt_params, verbose=False)
-    return res
+    gpt_params.update({"messages": messages})
+
+    response = client.chat.completions.create(**gpt_params)
+    response = GenAIResponse(
+        text=response.choices[0].message.content,
+        total_tokens=response.usage.total_tokens,
+        model=model,
+        generation_id=generate_id(user_prompt)
+    )
+
+    return response
 
 def recs_generation(candidates):
     candidates = '\n'.join(["item_id: %s; review: %s" % (i['asin'], i['content']) for i in candidates])
     prompt = promt_generation(candidates)
-    generated_result = generate(prompt)
+    generated_result = generate(user_prompt=prompt)
     return generated_result['recs']
